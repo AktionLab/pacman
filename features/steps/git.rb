e5135fb4c8efd354b21c8ddd29f55aabe0f109df
Chris Boertien <chris@aktionlab.com>
2014-01-27 21:45:33 -0700

    use aruba default dirs
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- features/steps/git.rb
+++ features/steps/git.rb
@@ -1,9 +1,9 @@
 Then /^git has a commit$/ do
-  git = Git.open('sandbox/test')
+  git = Git.open('tmp/aruba/test')
   git.log.count.should_not be_zero
 end
 
 Then /^git has a remote named "([^"]+)" for "([^"]+)"$/ do |name,remote_repo|
-  git = Git.open('sandbox/test')
+  git = Git.open('tmp/aruba/test')
   git.remote(name).url.should == remote_repo
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -2,11 +2,6 @@ require 'aruba/cucumber'
 require 'rvm'
 require 'git'
 
-Before do
-  @dirs = ["sandbox"]
-end
-
 After do
   RVM.gemset.delete('test')
-  Dir['./sandbox/*'].each {|d| FileUtils.rm_rf(d)}
 end
